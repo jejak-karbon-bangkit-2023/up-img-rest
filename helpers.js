00fef0bf86843433f471e8edfa6a10781d533f6e
Micko-Wijayanto <micko.wijayanto@gmail.com>
2023-06-02 19:25:06 +0700

    initial commit

---
+++ helpers.js
@@ -0,0 +1,35 @@
+
+const util = require('util')
+const gc = require('./config')
+const bucket = gc.bucket('img-pros') // should be your bucket name
+const { format } = require('url');
+
+/**
+ *
+ * @param { File } object file object that will be uploaded
+ * @description - This function does the following
+ * - It uploads a file to the image bucket on Google Cloud
+ * - It accepts an object as an argument with the
+ *   "originalname" and "buffer" as keys
+ */
+const username = 'cek1';
+const uploadImage = (file) => new Promise((resolve, reject) => {
+  const { originalname, buffer } = file;
+
+  const blob = bucket.file(`${username}` + '_' +originalname.replace(/ /g, "_"));
+  const blobStream = blob.createWriteStream({
+    resumable: false
+  })
+  blobStream.on('finish', () => {
+    const publicUrl = format(
+      `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+    )
+    resolve(publicUrl)
+  })
+  .on('error', () => {
+    reject(`Unable to upload image, something went wrong`)
+  })
+  .end(buffer)
+})
+
+module.exports = uploadImage;
